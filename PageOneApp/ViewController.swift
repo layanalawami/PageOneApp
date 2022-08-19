683dcea98b92976db6e3dcffa28c730ea3be2611
Layan <layoona99889@gmail.com>
2022-08-19 18:19:38 +0300

    resources page done

--- PageOneApp/ViewController.swift
+++ PageOneApp/ViewController.swift
@@ -14,6 +14,41 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+ 
+    @IBAction func academicWebsite(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://academicearth.org/")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func khanAcademy(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.khanacademy.org/")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func goConqr(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.goconqr.com/")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func everNote(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://evernote.com/")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func studyStack(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.studystack.com/")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func schoolTraq(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"http://schooltraq.com/p/home/")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func hippoCampus(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.hippocampus.org/")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func easyBib(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.easybib.com/")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    
+    
+    
 }
 
